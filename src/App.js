a07a66b906728c429e6597d8ad789b535edae155
Alejandro <AleexSolis@hotmail.com>
2021-11-15 13:55:27 -0600

    init repository

--- src/App.js
+++ src/App.js
@@ -1,24 +1,160 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import moment from "moment";
+import {
+  Box,
+  Button,
+  Container,
+  Alert,
+  IconButton,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import BlockIcon from "@mui/icons-material/Block";
+import { TimePicker, ConfirmDialog } from "./components";
+import "./App.css";
+
+const { exec } = window.require("child_process");
+const os = window.require("os");
 
 function App() {
+  const [time, setTime] = useState(new Date());
+  const [open, setOpen] = useState(false);
+  const [openCancel, setOpenCancel] = useState(false);
+  const [alert, setAlert] = useState(undefined);
+  const [alertInfo, setAlertInfo] = useState(false);
+
+  const onChange = (time) => {
+    setTime(time);
+  };
+
+  const confirmShutdown = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+    setOpenCancel(false);
+  };
+
+  const onConfirm = () => {
+    let command;
+    switch (os.platform()) {
+      case "Windows_NT":
+        command = `shutdown -s -t ${moment(time).diff(new Date(), "s")}`;
+        break;
+      default:
+        command = `sudo shutdown -h ${moment(time).format("HH:mm")}`;
+        break;
+    }
+    exec(command, (error, stdout, stderr) => {
+      if (error || stderr) {
+        setAlert({
+          severity: "error",
+          description:
+            "Error when trying to program the shutdown of your computer.",
+        });
+        setAlertInfo(false);
+      }
+    });
+    setAlert({
+      severity: "success",
+      description: `Your computer will be shutdown at ${moment(time).format(
+        "LT"
+      )}.`,
+    });
+
+    onClose();
+    setAlertInfo(true);
+  };
+
+  const onConfirmCancel = () => {
+    let command;
+    switch (os.platform()) {
+      case "Windows_NT":
+        command = `shutdown /a`;
+        break;
+      default:
+        command = "sudo killall shutdown";
+        break;
+    }
+    exec(command, (error, stdout, stderr) => {
+      console.log(error);
+      console.log(stderr);
+      if (error || stderr) {
+        setAlert({
+          severity: "error",
+          title: "Error",
+          description:
+            "Error when trying to program the shutdown of your computer.",
+        });
+      }
+
+      onClose();
+      setAlertInfo(false);
+      setAlert(undefined);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container maxWidth="sm">
+      {alertInfo && <Alert
+        severity="info"
+        action={
+          <IconButton
+            aria-label="close"
+            color="inherit"
+            size="small"
+            onClick={onConfirmCancel}
+          >
+            <BlockIcon fontSize="inherit" />
+          </IconButton>
+        }
+      >
+        This computer is scheduled to shut down.
+      </Alert>}
+      {alert && (
+        <Alert
+          severity={alert.severity}
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => {
+                setAlert(undefined);
+              }}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {alert.description}
+        </Alert>
+      )}
+      <Box>
+        <TimePicker value={time} onChange={onChange} />
+      </Box>
+      <Box style={{ display: "flex", justifyContent: "center" }}>
+        <Button variant="contained" onClick={confirmShutdown}>
+          Schedule
+        </Button>
+      </Box>
+      <ConfirmDialog
+        open={open}
+        onClose={onClose}
+        onConfirm={onConfirm}
+        title="Confirm Shutdown"
+        description={`You are sure you want your computer to shut down at ${moment(
+          time
+        ).format("LT")}?`}
+      />
+      <ConfirmDialog
+        open={openCancel}
+        onClose={onClose}
+        onConfirm={onConfirmCancel}
+        title="Stop Shutdown"
+        description={`Are you sure you want to cancel the shutdown?`}
+      />
+    </Container>
   );
 }
 
---
+++ src/components/ConfirmDialog/ConfirmDialog.js
@@ -0,0 +1,34 @@
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+export default function AlertDialog({ open, onClose, onConfirm, title, description }) {
+  return (
+    <>
+      <Dialog
+        open={open}
+        onClose={onClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {title}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {description}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onClose}>Cancel</Button>
+          <Button onClick={onConfirm} autoFocus>
+            OK
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
